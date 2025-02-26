30b63ed415d81115f3d3516b85557fcbfa663494
Emely Pacheco <pachecoemely@gmail.com>
2025-02-26 15:11:57 -0500

    cambio formulario

---
+++ controlador.js
@@ -0,0 +1,4 @@
+//alert("estoy vinculado")
+let inputPesoUsuario=document.getElementById("pesousuario")
+let inputAlturaUsuario=document.getElementById("alturausuario")
+let botonFormulario=document.getElementById("boton")
